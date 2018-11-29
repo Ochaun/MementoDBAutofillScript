305c4f55abf637c47dbc1fb45f23fc18aab902bb
OEMarshall <ochaunmarshall@gmail.com>
2018-11-29 11:13:10 -0500

    Update autofill_script.js

--- autofill_script.js
+++ autofill_script.js
@@ -1,8 +1,31 @@
-var jsonresult = http().get("https://api.upcitemdb.com/prod/trial/lookup?upc=” + query);
+var jsonresult = http().get("https://api.upcitemdb.com/prod/trial/lookup?upc=" + term);
+
+var name = JSON.parse(jsonresult.body)["items"][0]["title"];
+var details = JSON.parse(jsonresult.body)["items"][0]["description"];
+var model = JSON.parse(jsonresult.body)["items"][0]["model"];
+
+log(typeof name)
+
+/*
+TODO figure out how to add this to description and check http status code
+var extra = "brand: " + JSON.parse(result.body)["items"][0]["brand"]
++
+"color: " + JSON.parse(result.body)["items"][0]["color"] +
+"size: " + JSON.parse(result.body)["items"][0]["size"] +
+"dimension: " + JSON.parse(result.body)["items"][0]["dimension"] +
+"weight: " + JSON.parse(result.body)["items"][0]["weight"];
+*/
 
 
-var name = JSON.parse(jsonresult.body)["items"]["title"];
-var details = JSON.parse(jsonresult.body)["items"]["description"];
-var model = JSON.parse(jsonresult.body)["items"]["model"];
+var stuff = new Object();
+var lst = [];
 
-result(name)
+stuff["title"] = name;
+stuff["description"] = details;
+stuff["model"] = model;
+
+lst.push(stuff);
+return lst;
+}
+
+result(getAPIdata(query));
