3ab184e3d81de7255997f0b39e075897f013be1e
OEMarshall <ochaunmarshall@gmail.com>
2018-11-29 11:12:18 -0500

    Update autofill_script.js

--- autofill_script.js
+++ autofill_script.js
@@ -6,11 +6,3 @@ var details = JSON.parse(jsonresult.body)["items"]["description"];
 var model = JSON.parse(jsonresult.body)["items"]["model"];
 
 result(name)
-
-
-
-
-####
-entry().set("Item name",name);
-entry().set("Description",details);
-entry().set("Model",model)
