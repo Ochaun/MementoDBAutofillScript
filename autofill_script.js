ba04cf841a5ae821bd9c1efaf2a06860cfb92531
Ochaun Marshall <ochaunmarshall@gmail.com>
2026-01-15 22:59:55 -0500

    Update autofill_script.js
    Update with Human generated code added from around 2021-2025. This was what was in prod

--- autofill_script.js
+++ autofill_script.js
@@ -1,41 +1,88 @@
-function getAPIdata(term){
-	var jsonresult = http().get("https://api.upcitemdb.com/prod/trial/lookup?upc=" + term);
-
-	if (jsonresult.code == 200){
-		var name = JSON.parse(jsonresult.body)["items"][0]["title"];
-		var details = JSON.parse(jsonresult.body)["items"][0]["description"];
-		var model = JSON.parse(jsonresult.body)["items"][0]["model"];
-
-		//Todo: make these into different fields
-		var extra = "brand :"+ JSON.parse(jsonresult.body)["items"][0]["brand"] +
-		"color: " + JSON.parse(jsonresult.body)["items"][0]["color"] +
-		"size: " + JSON.parse(jsonresult.body)["items"][0]["size"] +
-		"dimension: " + JSON.parse(jsonresult.body)["items"][0]["dimension"] +
-		"weight: " + JSON.parse(jsonresult.body)["items"][0]["weight"];
-
-		var stuff = new Object();
-		var lst = [];
-
-		stuff["title"] = name;
-		stuff["description"] = details + extra;
-		stuff["model"] = model;
-
-		lst.push(stuff);
-		return lst;
-
-	} else {
-		var name = "ERROR with getting info from api!";
-		var stuff = new Object();
-		var lst = [];
-
-		stuff["title"] = name;
-		stuff["description"] = name;
-		stuff["model"] = name;
-
-		lst.push(stuff);
-		return lst;
-	}
-
+function getAPIdata(e) {
+  var t = http().get("https://api.upcitemdb.com/prod/trial/lookup?upc=" + e);
+  if (200 == t.code) {
+    var r = JSON.parse(t.body).items[0];
+    return r.name = r.title,
+      r.details = r.description,
+      r.img = r.images[0],
+      r.description = r.details + "\nbrand: " + r.brand + "\ncolor: " + r.color + "\nsize: " + r.size + "\ndimension: " + r.dimension + "\nweight: " + r.weight + "\ncategory: " + r.category,
+      r.category = setLibCategory(r.category),
+      [r];
+  }
+  return [{
+    title: "ERROR with getting info from api!",
+    description: "ERROR with getting info from api!"
+  }];
 }
 
+function setLibCategory(e) {
+  switch (!0) {
+    case /Pet Sup/.test(e):
+      return "Pet";
+    case /Baby/.test(e):
+      return "Baby";
+    case /> Jewelery/.test(e):
+      return "Jewelry";
+    case /Clothing >|Clothing Accessories|Costumes /.test(e):
+      return "Clothing";
+    case /Hobbies/.test(e):
+      return "Art";
+    case /Party & Celebration|> Decor >/.test(e):
+      return "Decorations";
+    case /Lighting/.test(e):
+      return "Lamps";
+    case/Cameras/.test(e):
+      return "Camera";
+    case /Electronics/.test(e):
+      return "Electronics";
+    case /Food,/.test(e):
+      return "Food";
+    case /Furni/.test(e):
+      return "Furniture";
+    case /Hardw/.test(e):
+      return "Tools";
+    case /Health Care /.test(e):
+      return "Health";
+    case /Health/.test(e):
+      return "Beauty";
+    case /Household Appliances/.test(e):
+      return "Small Appliances";
+    case /Cleaning Supplies|Paper Products|Garbage Bags|Shoe Care|Waste /.test(e):
+      return "Cleaning Supplies";
+    case /Laund/.test(e):
+      return "Laundry";
+    case /Food & Beverage C/.test(e):
+      return "Organizers";
+    case /Kitchen Appliance A/.test(e):
+      return "Miscellaneous";
+    case /Kitchen A/.test(e):
+      return "Small Appliances";
+    case /Kitchen O/.test(e):
+      return "Organizers";
+    case /Cookware |Kitchen Too/.test(e):
+      return "Cooking";
+    case /Kitchen & Dining >/.test(e):
+      return "Dishes";
+    case /Lawn & G/.test(e):
+      return "Gardening";
+    case /Plants|Lighting/.test(e):
+      return "Lamps";
+    case /Linens/.test(e):
+      return "Linens";
+    case /Lug/.test(e):
+      return "Organizers";
+    case /Media/.test(e):
+      return "Media";
+    case /Office/.test(e):
+      return "Office Supplies";
+    case /Sporti/.test(e):
+      return "Sports";
+    case /Toys/.test(e):
+      return "Toys";
+    case /Veh/.test(e):
+      return "Automotive";
+    default:
+      return "Any";
+  }
+}
 result(getAPIdata(query));
